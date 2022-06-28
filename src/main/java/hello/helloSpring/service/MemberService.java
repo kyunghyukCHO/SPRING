502f3a180b2497f86a3900dfda38cda7f374d467
kyunghyukcho <rudgur1996@hanmail.net>
2022-06-28 18:29:32 +0900

    third commit

--- src/main/java/hello/helloSpring/service/MemberService.java
+++ src/main/java/hello/helloSpring/service/MemberService.java
@@ -4,6 +4,7 @@ import hello.helloSpring.domain.Member;
 import hello.helloSpring.repository.MemberRepository;
 import hello.helloSpring.repository.MemoryMemberRepository;
 
+import java.util.*;
 import java.util.Optional;
 
 public class MemberService {
@@ -13,13 +14,24 @@ public class MemberService {
     // 회원가입
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원이 있으면 안됩니다.
-        Optional<Member> result = memberrepository.findByName(member.getName());
-        result.ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        });
+        //Optional<Member> result = memberrepository.findByName(member.getName());
+        validateDuplicateMember(member); // 중복 회원 검증
         memberrepository.save(member);
         return member.getId();
     }
+    private void validateDuplicateMember(Member member) {
+        memberrepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberrepository.findAll();
+    }
 
+    public Optional<Member> findOne(Long memberId) {
+        return memberrepository.findById(memberId);
+    }
 
 }
